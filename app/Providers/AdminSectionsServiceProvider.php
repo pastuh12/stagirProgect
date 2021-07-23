1fd9db99b39fe21593693aadc4c814ec4fbc8709
ilya_ <ilyatarasov75@gmail.com>
2021-07-24 00:57:16 +0300

    добавлена основа админки

---
+++ app/Providers/AdminSectionsServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use SleepingOwl\Admin\Providers\AdminSectionsServiceProvider as ServiceProvider;
+
+class AdminSectionsServiceProvider extends ServiceProvider
+{
+
+    /**
+     * @var array
+     */
+    protected $sections = [
+   // \App\Role::class => 'App\Http\Admin\Roles',
+    \App\Models\User::class => 'App\Http\Admin\Users',
+    ];
+
+    /**
+     * Register sections.
+     *
+     * @param \SleepingOwl\Admin\Admin $admin
+     * @return void
+     */
+    public function boot(\SleepingOwl\Admin\Admin $admin)
+    {
+    	//
+
+        parent::boot($admin);
+    }
+}
