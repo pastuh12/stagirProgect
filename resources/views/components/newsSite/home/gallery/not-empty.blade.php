f09a964bc2e193d74a443a3b770d0fc9ee089826
ilya_ <ilyatarasov75@gmail.com>
2021-08-13 15:20:22 +0300

    попробывал добавить изменения количества выводимых комментариев, но не вышло, пагинация подвела доработал верстку вьюшек, связанных с аунтификацией и авторизацией

--- resources/views/components/newsSite/home/gallery/not-empty.blade.php
+++ resources/views/components/newsSite/home/gallery/not-empty.blade.php
@@ -15,14 +15,11 @@
             </div>
             <button class="carousel-control-prev" type="button"
                     data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                <div>
                     <span class="carousel-control-prev-icon" aria-hidden="true" ></span>
 {{--                    <span class="visually-hidden">Предыдущая</span>--}}
-                </div>
             </button>
             <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Следующая</span>
             </button>
         </div>
     </div>
