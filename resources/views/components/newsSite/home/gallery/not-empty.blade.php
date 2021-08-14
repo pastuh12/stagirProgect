81b0f2f0d571e1055c6d11a85bb6115431d33292
ilya_ <ilyatarasov75@gmail.com>
2021-08-14 12:28:49 +0300

    слияние make-view-counter в изменения_в_миграциях_еще_не_доработанные

--- resources/views/components/newsSite/home/gallery/not-empty.blade.php
+++ resources/views/components/newsSite/home/gallery/not-empty.blade.php
@@ -7,19 +7,19 @@
 </div>
 <div class="row d-flex justify-content-center">
     <div class="col-10 ">
-        <div id="carouselExampleControls" class="carousel carousel-dark slide" data-bs-ride="carousel">
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner" style="">
                 @foreach ($galleries as $key => $gallery)
                     @include('components.newsSite.home.gallery.carousel-item')
                 @endforeach
             </div>
-            <button class="carousel-control-prev" type="button"
-                    data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true" ></span>
-{{--                    <span class="visually-hidden">Предыдущая</span>--}}
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Предыдущая</span>
             </button>
             <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Следующая</span>
             </button>
         </div>
     </div>
