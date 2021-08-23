7b75d8631aa969623b03f43456b20a90c3de5fbb
ilya_ <ilyatarasov75@gmail.com>
2021-08-23 19:51:26 +0300

    теперь, если пользователь авторизирован, то в правом углу появляется его аватарка и имя

--- resources/views/components/newsSite/nav.blade.php
+++ resources/views/components/newsSite/nav.blade.php
@@ -1,16 +1,21 @@
 <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light border border-info">
     <div class="container-fluid">
-        <a class="navbar-brand" href="{{route('home')}}">
-            <img src="{{asset('assets/img/logo.jpg')}}" alt="logo" width="30" height="24"
-                 class="d-inline-block align-text-top">
-            Bootstrap
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent" style="position: relative">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li>
+                    <a class="navbar-brand" href="{{route('home')}}">
+                        <img src="{{asset('assets/img/logo.jpg')}}" alt="logo" width="30" height="24"
+                             class="d-inline-block align-text-top">
+                        MyNews
+                    </a>
+                </li>
+                <li>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Link</a>
                 </li>
@@ -26,15 +31,38 @@
                         @endforeach
                     </ul>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('register')}}" tabindex="-1">Регистрация</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('login')}}" tabindex="-1">Вход</a>
-                </li>
-
             </ul>
+            <div class="d-flex" style="position: absolute; left: 88%">
+                <div class="account d-flex flex-row">
+                    @if(\Illuminate\Support\Facades\Auth::check())
+                        <div>
+                            <img class="nav-item avatar mr-2" src="{{asset(\Illuminate\Support\Facades\Auth::user()->avatar)}}"
+                                 alt="avatar" style="border-radius: 100px; border: 1px solid black;">
+                        </div>
+                        <div class="dropdown py-2" style="height: 32px">
+                            <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                {{\Illuminate\Support\Facades\Auth::user()->name}}
+                            </button>
+                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                <li>          <form method="POST" action="{{ route('logout') }}">
+                                        @csrf
+
+                                        <x-dropdown-link :href="route('logout')"
+                                                         onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                            Выйти
+                                        </x-dropdown-link>
+                                    </form></li>
+                            </ul>
+                        </div>
+                    @else
+                        <a class="nav-link" href="{{route('register')}}" tabindex="-1">Регистрация</a>
+
+                        <a class="nav-link" href="{{route('login')}}" tabindex="-1">Вход</a>
+                    @endif
+                </div>
+
+            </div>
         </div>
     </div>
 </nav>
