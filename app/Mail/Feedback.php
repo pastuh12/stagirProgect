017e25eae8e501930dea21c21a8c79c91ba26443
ilya_ <ilyatarasov75@gmail.com>
2021-08-31 03:36:37 +0300

    убран ненужный input

--- app/Mail/Feedback.php
+++ app/Mail/Feedback.php
@@ -1,41 +1,45 @@
 <?php
-//
-//namespace App\Mail;
-//
-//use Illuminate\Bus\Queueable;
-//use Illuminate\Contracts\Queue\ShouldQueue;
-//use Illuminate\Mail\Mailable;
-//use Illuminate\Queue\SerializesModels;
-//
-//class Feedback extends Mailable
-//{
-//    use Queueable, SerializesModels;
-//
-//    private string $name;
-//    private string $text;
-//
-//    /**
-//     * Create a new message instance.
-//     *
-//     * @return void
-//     */
-//    public function __construct(string $name, string $text)
-//    {
-//        $this->name = $name;
-//        $this->text = $text;
-//    }
-//
-//    /**
-//     * Build the message.
-//     *
-//     * @return $this
-//     */
-//    public function build()
-//    {
-//        return $this->view('view.name')
-//            ->with([
-//                'userName' => $this->name,
-//                'text' => $this->text,
-//            ]);
-//    }
-//}
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class Feedback extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    private string $name;
+    private string $text;
+    private string $email;
+
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(string $name, string $email, string $text)
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->text = $text;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.feedback')
+            ->with([
+                'userName' => $this->name,
+                'userEmail' => $this->email,
+                'text' => $this->text,
+            ]);
+    }
+}
--- resources/views/components/newsSite/pageGallery/addGallery.blade.php
+++ resources/views/components/newsSite/pageGallery/addGallery.blade.php
@@ -22,9 +22,6 @@
             <label for="title" class="form-label">Название</label>
             <input type="text" class="form-control" id="title" name="title" value="{{old('title')}}" required>
         </div>
-        <div class="mb-3" hidden>
-            <input type="password" class="form-control" name="author_id" value="{{auth()->user()->id}}" required>
-        </div>
         <div class="input-group mb-3">
             <input type="file" class="form-control" id="image" name="image">
         </div>
