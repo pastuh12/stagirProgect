a1fb82da41f50f20e922d0d76022efef77bfa9b3
ilya_ <ilyatarasov75@gmail.com>
2021-09-02 11:38:07 +0300

    В таблицу category добавлено новое поле parent Заработали фактории и сиды для Category, category_news и News

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class CategoryFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Category::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => Str::random(10),
+            'parent' => random_int(1, 3),
+        ];
+    }
+}
