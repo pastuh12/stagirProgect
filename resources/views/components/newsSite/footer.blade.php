5e5d0174f7867fb78f04b96ecf0f9c7eae88fa7b
ilya_ <ilyatarasov75@gmail.com>
2021-09-08 03:45:22 +0300

    больше контактной информации

--- resources/views/components/newsSite/footer.blade.php
+++ resources/views/components/newsSite/footer.blade.php
@@ -1,12 +1,16 @@
 <div class="container mt-5">
     <footer class="d-flex flex-wrap justify-content-around align-items-start py-3 mt-4 mb-0 border-top">
-        <ul class="nav justify-content-end">
-            <li class="nav-item"><a href="#header" class="nav-link px-2 text-muted">В начало</a></li>
-            <li class="nav-item"><a href="{{route('home')}}" class="nav-link px-2 text-muted">На главную</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
-            <li class="nav-item"><a href="#header" class="nav-link px-2 text-muted">В начало</a></li>
-            <li class="nav-item"><a href="{{route('home')}}" class="nav-link px-2 text-muted">На главную</a></li>
-        </ul>
+        <div class="col-6 d-flex flex-column justify-content-start">
+            <ul class="nav justify-content-start mb-3">
+                <li class="nav-item"><a href="#header" class="nav-link px-0 text-muted">В начало</a></li>
+                <li class="nav-item"><a href="{{route('home')}}" class="nav-link px-2 text-muted">На главную</a></li>
+            </ul>
+            <ul class="nav flex-column">
+                <li class="nav-item">Контактный номер: <a href="tel:+7 (918) 965 07 38">+7 (918)-965-07-38</a></li>
+                <li class="nav-item">
+                    Контактная почта: <a href="mailto:ilyatarasov75@gmail.com">ilyatarasov75@gmail.com</a></li>
+            </ul>
+        </div>
         @include('components.newsSite.feedback-form')
     </footer>
 </div>
